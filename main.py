e3b5a47d4220ca160055e13eaa260eae591ca06c
fpellicano <fpellicano@newhaven.edu>
2025-09-19 13:27:00 -0400

    Added description comment

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('World')
+    print_hi('World') # Initialization to show to the user
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
