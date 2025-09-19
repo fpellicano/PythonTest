75f8addfc3f15d2e127f4a800f5bcddefcb27866
Yashgamer-x <yashdesai121212@gmail.com>
2025-09-19 13:43:50 -0400

    Please make the pull

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
+ print_hi('class')
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
